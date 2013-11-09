0a4459df122deb01268b668d5d937fc6384727d6
Vishnu Prathish <vishnu.prathish@gmail.com>
2013-11-09 01:49:25 -0500

    t

---
+++ ShoutProject/Shout/src/main/java/com/ShoutApp/shout/adapter/TabsPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.ShoutApp.shout.adapter;
+
+/**
+ * Created by vishnu on 11/8/2013.
+ */
+import com.ShoutApp.shout.GamesFragment;
+import com.ShoutApp.shout.MoviesFragment;
+import com.ShoutApp.shout.TopRatedFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsPagerAdapter extends FragmentPagerAdapter {
+
+    public TabsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int index) {
+
+        switch (index) {
+            case 0:
+                // Top Rated fragment activity
+                return new TopRatedFragment();
+            case 1:
+                // Games fragment activity
+                return new GamesFragment();
+            case 2:
+                // Movies fragment activity
+                return new MoviesFragment();
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        // get item count - equal to number of tabs
+        return 3;
+    }
+
+}
